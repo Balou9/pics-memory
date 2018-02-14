2de73938d031bfbb994933385f5e08b4b96057de
Emmanuel Owosekun <emmanuel.owosekun@annalect.com>
2018-02-14 19:17:30 +0100

    test modi

--- test.js
+++ test.js
@@ -3,8 +3,7 @@ var pics_memory = require('./index.js')
 
 
 tape('Test if function output', function (t) {
-
   var input = pics_memory
-  t.true(typeof console.log(input), 'object', 'is an object')
+  t.true(typeof input, 'object', 'is an object')
   t.end()
 })
